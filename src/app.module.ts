5afc2d8f4f287a49f06a619ee5524fdb2d48772e
jst4rk <dayron.alfa@gmail.com>
2023-03-27 08:14:00 -0400

    feat: update mongodb url

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
+    // I'm goind to hardcode here the mongodb url for testing
+    MongooseModule.forRoot('mongodb+srv://dayronalfa:<password>@cluster0.vl6unu2.mongodb.net/musalasoft?retryWrites=true&w=majority'),
     MongooseModule.forRoot('mongodb://localhost/musalasoft'),
     GatewaysModule,
     DevicesModule
