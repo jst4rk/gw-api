bfbb632085bf2a2ead8f65ee57e5d703e7582716
jst4rk <dayron.alfa@gmail.com>
2023-03-27 08:16:50 -0400

    chore: update comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    // I'm goind to hardcode here the mongodb url for testing
+    // * I'm goind to hardcode here the mongodb url for testing
     MongooseModule.forRoot('mongodb+srv://dayronalfa:<password>@cluster0.vl6unu2.mongodb.net/musalasoft?retryWrites=true&w=majority'),
-    MongooseModule.forRoot('mongodb://localhost/musalasoft'),
+    // * Uncomment this for test in a local environment
+    // * MongooseModule.forRoot('mongodb://localhost/musalasoft'),
     GatewaysModule,
     DevicesModule
   ],
